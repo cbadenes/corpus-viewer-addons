3d7e664205eb9c324e530b824a366be0c564ad75
Carlos Badenes <cbadenes@gmail.com>
2018-05-03 13:00:22 +0200

    train a topic model from CORDIS corpus

---
+++ src/main/java/es/gob/minetad/corpus/CorpusManager.java
@@ -0,0 +1,46 @@
+package es.gob.minetad.corpus;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
+
+import java.io.IOException;
+
+/**
+ * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
+ */
+public abstract class CorpusManager {
+
+    static{
+        Unirest.setDefaultHeader("Accept", "application/json");
+        Unirest.setDefaultHeader("Content-Type", "application/json");
+
+        com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        jacksonObjectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        Unirest.setObjectMapper(new ObjectMapper() {
+
+            public <T> T readValue(String value, Class<T> valueType) {
+                try {
+                    return jacksonObjectMapper.readValue(value, valueType);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public String writeValue(Object value) {
+                try {
+                    return jacksonObjectMapper.writeValueAsString(value);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+
+    abstract void create();
+
+    abstract void train();
+}
