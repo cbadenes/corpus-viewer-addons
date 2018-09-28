8ef27b2b93f46a48c92a9dc06e2b8a3f949fe601
Carlos Badenes <cbadenes@gmail.com>
2018-09-28 10:59:52 +0200

    test JSD simmilaritiy based on Lucene index

--- src/main/java/es/gob/minetad/metric/JensenShannon.java
+++ src/main/java/es/gob/minetad/metric/JensenShannon.java
@@ -5,7 +5,9 @@ import com.google.common.primitives.Doubles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
@@ -15,9 +17,14 @@ public class JensenShannon {
 
     private static final Logger LOG = LoggerFactory.getLogger(JensenShannon.class);
 
+    public static final double log2 = Math.log(2.0D);
 
     public static double similarity(List<Double> v1, List<Double> v2){
         return similarity(Doubles.toArray(v1), Doubles.toArray(v2));
+    }
+
+    public static double similarity(Map<String,Double> v1, Map<String,Double> v2){
+        return 1 - divergence(v1,v2);
     }
 
     public static double similarity(double[] v1, double[] v2){
@@ -30,6 +37,38 @@ public class JensenShannon {
 
     public static double divergence(double[] v1, double[] v2){
         return Maths.jensenShannonDivergence(v1,v2);
+    }
+
+    public static double divergence(Map<String,Double> v1, Map<String,Double> v2){
+        double[] average = new double[v1.size()];
+
+        Map<String,Double> p2 = new HashMap<>();
+        int index = 0;
+        for(String key: v1.keySet()){
+            if (!v2.containsKey(key)) continue;
+            average[index++] += (v1.get(key) + v2.get(key)) / 2.0D;
+            p2.put(key,v2.get(key));
+        }
+
+        return (klDivergence(v1, average) + klDivergence(p2, average)) / 2.0D;
+    }
+
+    public static double klDivergence(Map<String,Double> p1, double[] p2) {
+
+        double klDiv = 0.0D;
+
+        int index = 0;
+        for(String key: p1.keySet()){
+            if(p1.get(key) != 0.0D) {
+                if(p2[index] == 0.0D) {
+                    return 1.0D / 0.0;
+                }
+
+                klDiv += p1.get(key) * Math.log(p1.get(key) / p2[index++]);
+            }
+        }
+
+        return klDiv / log2;
     }
 
 }
--- src/test/java/es/gob/minetad/lucene/DocTopicsTest.java
+++ src/test/java/es/gob/minetad/lucene/DocTopicsTest.java
@@ -1,6 +1,7 @@
 package es.gob.minetad.lucene;
 
 import com.google.common.base.Strings;
+import es.gob.minetad.doctopic.CRDCIndex;
 import es.gob.minetad.doctopic.DocTopicsUtil;
 import es.gob.minetad.metric.JensenShannon;
 import es.gob.minetad.model.Combination;
@@ -37,6 +38,8 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -53,34 +56,59 @@ public class DocTopicsTest {
 
     public static final String DOCTOPICS_PATH = "https://delicias.dia.fi.upm.es/nextcloud/index.php/s/iQx4Zy2dPcY84Sd/download";
 
-    public static final Integer NUM_DOCS = 10000;
+    public static final Integer NUM_DOCS = 1000;
 
     private static FSDirectory directory;
     private static DirectoryReader indexReader;
-    
-    private static float epsylon;
-    private static float multiplicationFactor;
 
-    private static AtomicInteger counter;
-    private static Integer numTopics;
+    private static List<Double> SAMPLE_VECTOR   = Arrays.asList(0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.026250000000000002,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.026250000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.03875,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.026250000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.05125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.03875,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.026250000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.05125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.026250000000000002,0.00125,0.026250000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.013750000000000002,0.013750000000000002,0.00125,0.013750000000000002,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125,0.00125);
 
-    private static List<Double> SAMPLE_VECTOR;
+    private static Integer numTopics            = SAMPLE_VECTOR.size();
+    private static float epsylon                = 1f / numTopics;
+    private static float multiplicationFactor   = Double.valueOf(1*Math.pow(10,String.valueOf(numTopics).length()+1)).floatValue();
+    private static CRDCIndex crdcIndex          = new CRDCIndex(numTopics,0.001,100000);
 
+    private static String vector2String(List<Double> vector){
+        return DocTopicsUtil.getVectorString(vector, multiplicationFactor, epsylon);
+//        return crdcIndex.toString(vector);
+    }
+
+    private static List<Double> string2Vector(String vector){
+        return DocTopicsUtil.getVectorFromString(vector, multiplicationFactor, numTopics, epsylon);
+//        return crdcIndex.toVector(vector);
+    }
+
+    private static Map<String,Double> string2map(String vector){
+        Map<String,Double> vectorMap = new ConcurrentHashMap<>();
+        Arrays.stream(vector.split(" ")).parallel().forEach(t -> vectorMap.put(StringUtils.substringBefore(t,"|"),Double.valueOf(StringUtils.substringAfter(t,"|"))/multiplicationFactor));
+        return vectorMap;
+    }
 
     @BeforeClass
     public static void setup() throws IOException {
         File indexFile = new File("output/doctopics");
-        if (indexFile.exists()) indexFile.delete();
+        directory = FSDirectory.open(indexFile.toPath());
+//        if (indexFile.exists()) indexFile.delete();
+        if (!indexFile.exists()) createIndex(indexFile);
+        indexReader      = DirectoryReader.open(directory);
+        LOG.info("num docs indexed: " + indexReader.numDocs());
+        LOG.info("max docs indexed: " + indexReader.maxDoc());
+
+
+//        List<Map.Entry<String, Double>> topSimilars = similarities.entrySet().parallelStream().sorted((a, b) -> -a.getValue().compareTo(b.getValue())).limit(10).collect(Collectors.toList());
+//        topSimilars.forEach(entry -> LOG.info("- " + entry.getKey() + " [" + entry.getValue() + "]"));
+    }
+
+    private static void createIndex(File indexFile) throws IOException {
         LOG.info("creating a file system directory for index");
         indexFile.getParentFile().mkdirs();
-        directory = FSDirectory.open(indexFile.toPath());
         IndexWriterConfig writerConfig = new IndexWriterConfig(new DocTopicAnalyzer());
         writerConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
         writerConfig.setRAMBufferSizeMB(500.0);
         IndexWriter writer = new IndexWriter(directory, writerConfig);
 
         LOG.info("indexing documents..");
-        counter = new AtomicInteger(0);
+        AtomicInteger counter = new AtomicInteger(0);
 
         ConcurrentLinkedQueue<Double> representationLengths = new ConcurrentLinkedQueue();
         BufferedReader doctopicsReader = ReaderUtils.from(DOCTOPICS_PATH);
@@ -99,13 +127,8 @@ public class DocTopicsTest {
                     for (int i=1; i<result.length; i++){
                         shape.add(Double.valueOf(result[i]));
                     }
-                    numTopics             = shape.size();
-                    epsylon               = 1f / numTopics;
-                    multiplicationFactor  = Double.valueOf(1*Math.pow(10,String.valueOf(numTopics).length()+1)).floatValue();
 
-                    if (SAMPLE_VECTOR == null) SAMPLE_VECTOR = shape;
-
-                    String stringTopics = DocTopicsUtil.getVectorString(shape, multiplicationFactor, epsylon);
+                    String stringTopics = vector2String(shape);
                     org.apache.lucene.document.Document luceneDoc = DocumentFactory.newDocId(id, stringTopics);
                     representationLengths.add(Double.valueOf(StringUtils.countMatches(stringTopics,"|")));
                     writer.addDocument(luceneDoc);
@@ -132,14 +155,9 @@ public class DocTopicsTest {
                 + ChronoUnit.MINUTES.between(startTime, endTime) % 60 + "min "
                 + (ChronoUnit.SECONDS.between(startTime, endTime) % 60) + "secs "
                 + (ChronoUnit.MILLIS.between(startTime, endTime) % 60) + "msecs";
-
-
-        indexReader      = DirectoryReader.open(directory);
-        LOG.info("num docs read: " + counter.get());
-        LOG.info("num docs indexed: " + indexReader.numDocs());
-        LOG.info("max docs indexed: " + indexReader.maxDoc());
         LOG.info("Index created in: " + elapsedTime);
         LOG.info("num topics stored stats: " + new Stats(new ArrayList<Double>(representationLengths)));
+        LOG.info("num docs read: " + counter.get());
     }
 
     @AfterClass
@@ -179,7 +197,7 @@ public class DocTopicsTest {
         mlt.setMinDocFreq(1);
         mlt.setAnalyzer(new DocTopicAnalyzer());
 
-        Reader stringReader = new StringReader(DocTopicsUtil.getVectorString(SAMPLE_VECTOR, multiplicationFactor, epsylon));
+        Reader stringReader = new StringReader(vector2String(SAMPLE_VECTOR));
 
         Query mltQuery = mlt.like(TopicIndexFactory.FIELD_NAME, stringReader);
 
@@ -190,7 +208,7 @@ public class DocTopicsTest {
 
 
     private void query(String id, IndexSearcher searcher) throws ParseException, IOException {
-        String queryString = DocTopicsUtil.getVectorString(SAMPLE_VECTOR, multiplicationFactor, epsylon);
+        String queryString = vector2String(SAMPLE_VECTOR);
 
         QueryParser parser = new QueryParser(TopicIndexFactory.FIELD_NAME, new DocTopicAnalyzer());
         Query query = parser.parse(queryString);
@@ -200,28 +218,38 @@ public class DocTopicsTest {
     }
 
     private void search(String id, IndexSearcher searcher, Query query) throws IOException {
+        List<Double> v1 = SAMPLE_VECTOR;
+        Map<String, Double> m1 = string2map(vector2String(v1));
         String description = "Searching by '" + id + "' ";
         LOG.info(description + Strings.repeat("-",100-description.length()));
         Instant startTime = Instant.now();
-        TopDocs results = searcher.search(query, counter.get());
+        TopDocs results = searcher.search(query, indexReader.numDocs());
 
+        Instant pstartTime = Instant.now();
         List<Score> topDocs = Arrays.stream(results.scoreDocs).parallel().map(scoreDoc -> {
             try {
                 org.apache.lucene.document.Document docIndexed = indexReader.document(scoreDoc.doc);
                 String vectorString = String.format(docIndexed.get(TopicIndexFactory.FIELD_NAME));
                 if (Strings.isNullOrEmpty(vectorString)) return new Score(0.0, new Document(), new Document());
-                return new Score(JensenShannon.similarity(SAMPLE_VECTOR, DocTopicsUtil.getVectorFromString(vectorString, multiplicationFactor, numTopics, epsylon)), new Document("ref"), new Document(String.format(docIndexed.get(TopicIndexFactory.DOC_ID))));
+
+//                Map<String, Double> m2 = string2map(vectorString);
+                List<Double> v2 = string2Vector(vectorString);
+                return new Score(JensenShannon.similarity(v1, v2), new Document("ref"), new Document(String.format(docIndexed.get(TopicIndexFactory.DOC_ID))));
             } catch (Exception e) {
                 e.printStackTrace();
                 return new Score(0.0, new Document(), new Document());
             }
-        }).filter(s -> s.getValue() > 0.5).sorted((a, b) -> -a.getValue().compareTo(b.getValue())).limit(10).collect(Collectors.toList());
-
+        }).filter(s -> s.getValue() > 0.7).sorted((a, b) -> -a.getValue().compareTo(b.getValue())).limit(10).collect(Collectors.toList());
+        topDocs.size();
+        Instant pEndTime = Instant.now();
+        LOG.info("Similarities Time: " + + ChronoUnit.MINUTES.between(pstartTime, pEndTime) % 60 + "min "
+                + (ChronoUnit.SECONDS.between(pstartTime, pEndTime) % 60) + "secs "
+                + (ChronoUnit.MILLIS.between(pstartTime, pEndTime) % 1000) + "msecs");
         Instant globalEndTime = Instant.now();
-        String globalElapsedTime = ChronoUnit.HOURS.between(startTime, globalEndTime) + "hours "
+        String globalElapsedTime =
                 + ChronoUnit.MINUTES.between(startTime, globalEndTime) % 60 + "min "
                 + (ChronoUnit.SECONDS.between(startTime, globalEndTime) % 60) + "secs "
-                + (ChronoUnit.MILLIS.between(startTime, globalEndTime) % 60) + "msecs";
+                + (ChronoUnit.MILLIS.between(startTime, globalEndTime) % 1000) + "msecs";
         LOG.info("Total Time: " + globalElapsedTime);
         LOG.info("Total Hits: " + results.totalHits);
 
