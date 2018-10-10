75ae3a39de48e5e213b6f56d446b66001ed32cbb
Carlos Badenes <cbadenes@gmail.com>
2018-10-10 13:34:51 +0200

    added bruteForce into global test

--- doctopics.sh
+++ doctopics.sh
@@ -4,4 +4,4 @@ mvn -DargLine="-Xmx4096m" -Dtest=BooleanByDTFEval test
 mvn -DargLine="-Xmx4096m" -Dtest=BooleanByHashEval test
 mvn -DargLine="-Xmx4096m" -Dtest=MLTByDTFEval test
 mvn -DargLine="-Xmx4096m" -Dtest=MLTByHashEval test
-
+mvn -DargLine="-Xmx4096m" -Dtest=BruteForceEval test
