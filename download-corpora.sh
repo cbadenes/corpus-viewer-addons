79541f4fb48ec4b60c0afd96e28bfb57e272827d
Carlos Badenes <cbadenes@gmail.com>
2018-11-26 11:44:11 +0100

    updated references and added DTM and CTM outputs

--- download-corpora.sh
+++ download-corpora.sh
@@ -12,9 +12,13 @@ curl -o corpora/cordis/doctopics-70.csv.gz https://delicias.dia.fi.upm.es/nextcl
 #curl -o corpora/wikipedia/doctopics-120.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/qJRqT3mnyPjTHjE/download
 #curl -o corpora/wikipedia/doctopics-350.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/QMjTxEoHr5CTALx/download
 #
-#echo "downloading PATSTAT corpus"
-#mkdir -p corpora/patstat
-#curl -o corpora/patstat/documents.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/zc3eeoanyZRac4G/download
-#curl -o corpora/patstat/bows.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/RqwBJ8PnGsT5Rgw/download
-#curl -o corpora/patstat/doctopics-250.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/mG5Lwsii2CosERa/download
-#curl -o corpora/patstat/doctopics-750.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/kTD8QEagJEyff3z/download
+echo "downloading Patents corpus"
+mkdir -p corpora/patents
+#curl -o corpora/patents/documents.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/zc3eeoanyZRac4G/download
+#curl -o corpora/patents/bows.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/RqwBJ8PnGsT5Rgw/download
+#curl -o corpora/patents/doctopics-250.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/mG5Lwsii2CosERa/download
+#curl -o corpora/patents/doctopics-750.csv.gz https://delicias.dia.fi.upm.es/nextcloud/index.php/s/kTD8QEagJEyff3z/download
+curl -o corpora/patents/dtm-15.zip https://delicias.dia.fi.upm.es/nextcloud/index.php/s/cLBX3yTYDCgdGJJ/download
+unzip corpora/patents/dtm-15.zip -d corpora/patents && rm corpora/patents/dtm-15.zip
+curl -o corpora/patents/ctm-20.zip https://delicias.dia.fi.upm.es/nextcloud/index.php/s/c7dgNtiPAGKKcZ9/download
+unzip corpora/patents/ctm-20.zip -d corpora/patents && rm corpora/patents/ctm-20.zip
