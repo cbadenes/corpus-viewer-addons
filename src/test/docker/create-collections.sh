87410ba2e30b989da5be284c36605769a8d303f6
anagalonso <ana.gonzalez@satec.es>
2018-12-13 16:37:22 +0100

    Add files via upload
    Mods

--- src/test/docker/create-collections.sh
+++ src/test/docker/create-collections.sh
@@ -1,13 +1,13 @@
 docker exec -it solr solr create_collection -c corpora -shards 1 -replicationFactor 1
-docker exec -it solr solr create_collection -c cordis-documents -shards 1 -replicationFactor 1
+docker exec -it solr solr create_collection -c cordis-documents -d documents -shards 1 -replicationFactor 1
 docker exec -it solr solr create_collection -c cordis-doctopics -d sesiad-config -shards 1 -replicationFactor 1
 docker exec -it solr solr create_collection -c cordis-topicdocs -d sesiad-config -shards 1 -replicationFactor 1
 docker exec -it solr solr create_collection -c cordis-similarities -d sesiad-config -shards 1 -replicationFactor 1
-docker exec -it solr solr create_collection -c wikipedia-documents -shards 1 -replicationFactor 1
+docker exec -it solr solr create_collection -c wikipedia-documents -d documents -shards 1  -replicationFactor 1
 docker exec -it solr solr create_collection -c wikipedia-doctopics -d sesiad-config -shards 1 -replicationFactor 1
 docker exec -it solr solr create_collection -c wikipedia-topicdocs -d sesiad-config -shards 1 -replicationFactor 1
 docker exec -it solr solr create_collection -c wikipedia-similarities -d sesiad-config -shards 1 -replicationFactor 1
-docker exec -it solr solr create_collection -c patents-documents -shards 1 -replicationFactor 1
+docker exec -it solr solr create_collection -c patents-documents -d documents -shards 1 -replicationFactor 1
 docker exec -it solr solr create_collection -c patents-doctopics -d sesiad-config -shards 1 -replicationFactor 1
 docker exec -it solr solr create_collection -c patents-topicdocs -d sesiad-config -shards 1 -replicationFactor 1
 docker exec -it solr solr create_collection -c patents-similarities -d sesiad-config -shards 1 -replicationFactor 1
